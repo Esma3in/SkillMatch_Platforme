eccf4dcd317ff142920efbb4d8b62840d45e79fc
ismail ait el hadj <ismail.aitelhadj2003@gmail.com>
2025-04-14 22:21:54 +0000

    factories smail v3

--- SkillMatch/database/factories/RoadmapFactory.php
+++ SkillMatch/database/factories/RoadmapFactory.php
@@ -16,18 +16,11 @@ class RoadmapFactory extends Factory
      *
      * @return array<string, mixed>
      */
-<<<<<<< HEAD:SkillMatch/database/factories/AdministratorFactory.php
-    public function definition()
-    {
-        return [
-            'user_id' => User::factory(),
-=======
     public function definition(): array
     {
         return [
              'skill_id'=>Skill::factory(),
              'candidate_id'=>Candidate::factory()
->>>>>>> 3cf2cfaf55f0fd0d58a1b907f79a51450a9fc90d:SkillMatch/database/factories/RoadmapFactory.php
         ];
     }
 }
--- SkillMatch/database/factories/Serie_ChallengeFactory.php
+++ SkillMatch/database/factories/Serie_ChallengeFactory.php
@@ -6,15 +6,10 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\User ;
 
 /**
-<<<<<<<< HEAD:SkillMatch/database/factories/CandidateFactory.php
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Candidate>
  */
-class CandidateFactory extends Factory
-========
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Serie_Challenge>
- */
+
 class Serie_ChallengeFactory extends Factory
->>>>>>>> 3cf2cfaf55f0fd0d58a1b907f79a51450a9fc90d:SkillMatch/database/factories/Serie_ChallengeFactory.php
 {
     /**
      * Define the model's default state.
@@ -24,16 +19,12 @@ class Serie_ChallengeFactory extends Factory
     public function definition()
     {
         return [
-<<<<<<<< HEAD:SkillMatch/database/factories/CandidateFactory.php
-            'name' => fake()->name,
+            'name' => fake()->name(),
             'dateInscription' => fake()->date(),
             'fichiers' => fake()->filePath(),
             'utilisateur_id' => User::factory(),
-========
-            'name' => fake()->neme(),
-            'challenge' => substr(fake()->paragraph(),0,255), 
-            'number' => fake()->numberBetween(1, 100),
->>>>>>>> 3cf2cfaf55f0fd0d58a1b907f79a51450a9fc90d:SkillMatch/database/factories/Serie_ChallengeFactory.php
+            'challenge' => substr(fake()->paragraph(),0,255),
+            'number' => fake()->numberBetween(1, 9999),
         ];
     }
 }
