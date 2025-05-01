6e3fbab131955157b1d5cbe96199908432c0a9eb
Esma3in <ismail.aitelhadj2003@gmail.com>
2025-05-01 22:35:35 +0100

    'sidebaresAdmin1'

--- skillmatch-front-end/src/pages/adminHome.jsx
+++ skillmatch-front-end/src/pages/adminHome.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { api } from "../api/api";
 import NavbarCandidate from '../components/common/navbarCandidate';
-
+import Sidebar from "../components/common/sideBars/sidebarPlatforme";
 
 export default function AdminHome(){
     return (
         <>
             <NavbarCandidate />
-            <h1>helooooooo</h1>
+            <div className='flex' >
+                <Sidebar className='inline' />
+                <h1>helooooooo</h1>
+            </div>
         </>
     )
 }
